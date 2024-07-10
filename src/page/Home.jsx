d8f4b4f4b7815627d40745aec897dfa5a96d13b5
Pintu130 <pin2sharma907@gmail.com>
2024-07-10 20:08:41 +0530

    done

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
       <div className="bg-gray-950 text-white py-20 flex flex-col justify-center items-center mt-16 md:mt-24 xl:mt-32" >
         <div className="flex flex-wrap justify-center gap-10" data-aos="slide-up">
           {counters.map((counter, index) => (
-            <div key={index} className="border border-blue-950 rounded-md p-5 transition-transform duration-300 ease-in-out transform hover:scale-110">
+            <div key={index} className="border border-blue-950 rounded-md p-5 transition-transform duration-300 ease-in-out transform lg:hover:scale-110">
               <p className="flex justify-center text-yellow-500">
                 {counter?.icon}
               </p>
